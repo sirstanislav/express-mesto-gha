479d46fc3c9df844aec8db4eb291b72a7dcaca64
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-06-22 23:42:55 +0200

    update

--- errors/errors.js
+++ errors/errors.js
@@ -1,12 +1,8 @@
-function error(err, res, errorName, errorMessage) {
+module.exports.error = (err, res, errorName, errorMessage) => {
   if (err.name === errorName) {
     return res.status(400).send({
       message: errorMessage,
     });
   }
   return res.status(500).send({ message: '500 — Ошибка по умолчанию' });
-}
-
-module.exports = {
-  error,
 };
