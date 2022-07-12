7dfe290b0fd19fcc5fbd599f762be8ed31dd1335
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-12 03:51:13 +0200

    update

--- models/user.js
+++ models/user.js
@@ -42,8 +42,8 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password') // this — это модель login
+userSchema.statics.findUserByCredentials = (email, password) =>
+  this.findOne({ email }).select('+password') // this — это модель login
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Такого пользователя не существует'));
@@ -55,6 +55,5 @@ userSchema.statics.findUserByCredentials = function (email, password) {
         return user;
       });
     });
-};
 
 module.exports = mongoose.model('user', userSchema);
