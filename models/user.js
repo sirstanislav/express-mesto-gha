6023ba052154488fedd4b777c28fb198f0573a18
Stanislav Sorochan <stas.sorochan@gmail.com>
2022-07-14 12:12:25 +0200

    update

--- models/user.js
+++ models/user.js
@@ -20,11 +20,11 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (val) => REG_LINK.test(val),
       message: () => 'Неверная ссылка',
     },
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
